d5ad18466c21fb7fd1bed8f89420ca44c101d989
Bob <bob.e.baxter@gmail.com>
2019-09-07 13:07:13 -0500

    added vehicle and car classes

---
+++ MyGarage/Program.cs
@@ -0,0 +1,17 @@
+﻿using MyGarage.Vehicles;
+using System;
+
+namespace MyGarage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myCar = new Car();
+
+            // myCar.Refuel();
+            myCar.Drive();
+            myCar.Brake();
+        }
+    }
+}
---
+++ MyGarage/Vehicles/Vehicle.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyGarage.Vehicles
+{
+    class Vehicle
+    {
+        public int Fuel { get; set; }
+        public Color Color { get; set; }
+        public int PassengerOccupancy { get; set; }
+
+        public void Refuel()
+        {
+            Fuel = 100;
+        }
+
+        public void ForwardMovement(string movementName)
+        {
+            Fuel -= 10;
+
+            if (Fuel <= 0)
+            {
+                Fuel = 0;
+                Console.WriteLine("You are out of gas");
+                return;
+            }
+
+            Console.WriteLine($"You {movementName} and now have {Fuel} gas left.");
+        }
+
+        public void Brake()
+        {
+            Console.WriteLine("You have stopped.");
+        }
+    }
+
+    enum Color
+    {
+        Black,
+        Blue,
+        Grey,
+        Red,
+        White,
+    }
+}
